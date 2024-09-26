78934cfc5bf33d358a7effab68367d99d832ac20
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-26 15:41:10 +0700

    Refactor svgXML.tsx, add backIcon function

--- assets/svgXML.tsx
+++ assets/svgXML.tsx
@@ -11,6 +11,17 @@ export const test = (w?: any, h?: any, color?: any) => {
   return <SvgXml xml={xml} width={w} height={h} />;
 };
 
+export const backIcon = (w?: any, h?: any, color?: any) => {
+  const xml = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M15 4L7 12L15 20" stroke=${
+    color ?? '#76787E'
+  } stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+
+  return <SvgXml xml={xml} width={w} height={h} />;
+};
+
 export const goggleIcon = (w?: any, h?: any, color?: any) => {
   const xml = `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_3310_3994)">
--- views/init/PhoneVerify.tsx
+++ views/init/PhoneVerify.tsx
@@ -1,24 +1,27 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import React, {useEffect, useState} from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {container, vh, vw} from '../../services/styleSheet';
 import useStatusBar from '../../services/useStatusBar';
 import PhoneInput from 'react-native-phone-input';
 import CountryPicker from 'react-native-country-picker-modal';
+import {backIcon} from '../../assets/svgXML';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const PhoneVerify = () => {
   useStatusBar('#1A1A1A');
-
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
   const phoneRef = React.useRef<PhoneInput>(null);
-  const [pickerData, setPickerData] = useState<any>(null);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [countryCode, setCountryCode] = useState('VN');
   const [modalVisible, setModalVisible] = useState(false);
-
-  useEffect(() => {
-    if (phoneRef.current) {
-      setPickerData(phoneRef.current.getPickerData());
-    }
-  }, []);
 
   const onSelect = (selectedCountry: any) => {
     setCountryCode(selectedCountry.cca2);
@@ -35,10 +38,17 @@ const PhoneVerify = () => {
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.phoneContainer}>
-          <Text>Nhập số điện thoại</Text>
-          <Text>
-            Bạn sẽ nhận được mã gồm 4 chữ số để xác minh số điện thoại
-          </Text>
+          <TouchableOpacity
+            style={styles.btnBack}
+            onPress={() => navigation.goBack()}>
+            {backIcon(vw(7), vw(7))}
+          </TouchableOpacity>
+          <View style={{rowGap: vh(1)}}>
+            <Text style={styles.label}>Nhập số điện thoại</Text>
+            <Text style={styles.desp}>
+              Bạn sẽ nhận được mã gồm 4 chữ số để xác minh số điện thoại
+            </Text>
+          </View>
           <PhoneInput
             ref={phoneRef}
             onPressFlag={onPressFlag}
@@ -58,6 +68,9 @@ const PhoneVerify = () => {
               countryCode={'VN'}
             />
           )}
+          <TouchableOpacity style={styles.btnSendOTP}>
+            <Text style={styles.btnOtptxt}>Gửi mã</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -82,5 +95,37 @@ const styles = StyleSheet.create({
   },
   phoneContainer: {
     paddingHorizontal: vw(5),
+    flex: 1,
+    paddingVertical: vh(2),
+    rowGap: vh(2),
+  },
+  btnSendOTP: {
+    backgroundColor: '#FFED4B',
+    padding: vw(3),
+    borderRadius: 16,
+    alignItems: 'center',
+    marginVertical: vh(2),
+  },
+  btnOtptxt: {
+    color: '#1A1A1A',
+    fontWeight: '600',
+    fontSize: 14,
+  },
+  label: {
+    color: '#76787E',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  desp: {
+    color: '#666D80',
+    fontSize: 14,
+    fontWeight: '400',
+  },
+  btnBack: {
+    padding: vw(2),
+    borderWidth: 1,
+    borderColor: '#76787E',
+    borderRadius: 10,
+    alignSelf: 'flex-start',
   },
 });
