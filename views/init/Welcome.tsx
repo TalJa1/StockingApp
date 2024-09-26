6a48f12f73ef772ff892e7af336f291558e6ed2a
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-26 16:36:04 +0700

    Refactor Welcome view in App.tsx, update styles and add start button

--- views/init/Welcome.tsx
+++ views/init/Welcome.tsx
@@ -1,15 +1,37 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
-import {container} from '../../services/styleSheet';
+import {centerAll, container, vh, vw} from '../../services/styleSheet';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {stocklineIcon} from '../../assets/svgXML';
 
 const Welcome = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View>
-          <Text>Welcome</Text>
+      <ScrollView contentContainerStyle={{flex: 1, paddingHorizontal: vw(5)}}>
+        <View style={[centerAll, styles.centerStyle]}>
+          <View style={{marginBottom: vh(4)}}>
+            {stocklineIcon(vw(20), vw(20))}
+          </View>
+          <Text style={styles.label}>
+            Xin chào Agatha Bella!{' '}
+            <Image source={require('../../assets/init/wavingHand.png')} />
+          </Text>
+          <Text style={styles.label}>Chào mừng tới Stockline</Text>
+          <Text style={styles.desp}>Thật tuyệt khi có bạn ở đây</Text>
         </View>
+        <TouchableOpacity>
+          <View style={[centerAll, styles.btn]}>
+            <Text style={styles.btntxt}>Bắt đầu</Text>
+          </View>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
@@ -19,4 +41,24 @@ export default Welcome;
 
 const styles = StyleSheet.create({
   container: container,
+  centerStyle: {flex: 1, rowGap: vh(1.5)},
+  btn: {
+    paddingVertical: vh(2),
+    backgroundColor: '#FFED4B',
+    borderRadius: vw(2),
+    marginBottom: vh(2),
+  },
+  btntxt: {
+    color: '#1A1A1A',
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  label: {
+    color: 'white',
+    fontSize: 22,
+    fontWeight: '600',
+  },
+  desp: {
+    color: '#76787E',
+  },
 });
