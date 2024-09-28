563af9cec26a28522307f333192521f1b189980e
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-28 13:03:04 +0700

    Refactor ChartInfor component: Add header with back button and stock information

--- views/chart/ChartInfor.tsx
+++ views/chart/ChartInfor.tsx
@@ -1,9 +1,17 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
-import {container} from '../../services/styleSheet';
+import {centerAll, container, vh, vw} from '../../services/styleSheet';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBar';
-import {useRoute} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {backIcon} from '../../assets/svgXML';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const ChartInfor = () => {
   useStatusBar('#1A1A1A');
@@ -15,11 +23,25 @@ const ChartInfor = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <View>
-          <Text>ChartInfor</Text>
-        </View>
+        <Header />
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const Header: React.FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  return (
+    <View style={[centerAll, {paddingVertical: vh(2)}]}>
+      <TouchableOpacity
+        style={styles.backBtn}
+        onPress={() => {
+          navigation.goBack();
+        }}>
+        {backIcon(vw(6), vw(6), '#76787E')}
+      </TouchableOpacity>
+      <Text style={styles.headerTxt}>Thông tin cổ phiếu</Text>
+    </View>
   );
 };
 
@@ -27,4 +49,13 @@ export default ChartInfor;
 
 const styles = StyleSheet.create({
   container: container,
+  headerTxt: {color: '#FFFFFF', fontSize: 14, fontWeight: '600'},
+  backBtn: {
+    position: 'absolute',
+    left: vw(5),
+    borderWidth: 1,
+    borderColor: '#76787E',
+    borderRadius: 10,
+    padding: vw(1.5),
+  },
 });
