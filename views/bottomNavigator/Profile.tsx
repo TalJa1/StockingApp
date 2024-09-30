6694dd3f7a152110050a2f6dba2d438d9960a45f
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-30 11:38:34 +0700

    Refactor Profile component: Add account render component and account menu items with icons

--- views/bottomNavigator/Profile.tsx
+++ views/bottomNavigator/Profile.tsx
@@ -1,27 +1,63 @@
 /* eslint-disable react-native/no-inline-styles */
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {centerAll, container, vh, vw} from '../../services/styleSheet';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBar';
 import {ProfileRenderView} from '../../services/renderData';
 import {nextIcon} from '../../assets/svgXML';
+import {UserProfile} from '../../services/typeProps';
+import {loadData} from '../../services/storage';
 
 const Profile = () => {
   useStatusBar('#1A1A1A');
+  const [user, setUser] = useState<UserProfile>({
+    givenName: '',
+    familyName: '',
+    email: '',
+    name: '',
+    photoUrl: null,
+  });
+
+  const fetchUserData = async () => {
+    await loadData<UserProfile>('userLoginStorage')
+      .then(data => {
+        setUser(data);
+      })
+      .catch(() => {
+        console.log('Error');
+      });
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <Header />
-        <AccountRender />
+        <AccountRender data={user} />
         <Main />
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-const AccountRender: React.FC = () => {
-  return <View></View>;
+const AccountRender: React.FC<{ data: UserProfile }> = ({ data }) => {
+  const firstLetter = data.email ? data.email.charAt(0).toUpperCase() : 'A';
+
+  return (
+    <View style={styles.container2}>
+      <View style={styles.circle}>
+        <Text style={styles.circleText}>{firstLetter}</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.name}>{data.name}</Text>
+        <Text style={styles.email}>{data.email}</Text>
+      </View>
+    </View>
+  );
 };
 
 const Main: React.FC = () => {
@@ -86,5 +122,36 @@ const styles = StyleSheet.create({
   nextIcon: {
     width: 24,
     height: 24,
+  },
+  container2: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+  },
+  circle: {
+    width: vw(20),
+    height: vw(20),
+    borderRadius: vw(60),
+    backgroundColor: '#FFED4B',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 16,
+  },
+  circleText: {
+    color: '#1A1A1A',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  textContainer: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#FFFFFF',
+  },
+  email: {
+    fontSize: 12,
+    color: '#76787E',
   },
 });
