7e18c8ec088b6f92c5c44be12e5851f8998feeb0
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-01 09:56:02 +0700

    Refactor OtpVerify.tsx: Add back button and styling for phone verification screen

--- views/init/OtpVerify.tsx
+++ views/init/OtpVerify.tsx
@@ -1,14 +1,40 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
-import {container} from '../../services/styleSheet';
+import {container, vh, vw} from '../../services/styleSheet';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {backIcon} from '../../assets/svgXML';
+import useStatusBar from '../../services/useStatusBar';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const OtpVerify = () => {
+  useStatusBar('#1A1A1A');
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <View>
-          <Text>OtpVerify</Text>
+        <View style={styles.phoneContainer}>
+          <TouchableOpacity
+            style={styles.btnBack}
+            onPress={() => navigation.goBack()}>
+            {backIcon(vw(7), vw(7))}
+          </TouchableOpacity>
+          <View style={{rowGap: vh(1)}}>
+            <Text style={styles.label}>Nhập mã xác minh</Text>
+            <Text style={styles.desp}>
+              Chúng tôi đã gửi mã xác minh đến số điện thoại di động của bạn
+            </Text>
+          </View>
+
+          <TouchableOpacity style={styles.btnSendOTP}>
+            <Text style={styles.btnOtptxt}>Xác minh tài khoản</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -19,4 +45,39 @@ export default OtpVerify;
 
 const styles = StyleSheet.create({
   container: container,
+  phoneContainer: {
+    paddingHorizontal: vw(5),
+    flex: 1,
+    paddingVertical: vh(2),
+    rowGap: vh(2),
+  },
+  btnSendOTP: {
+    backgroundColor: '#FFED4B',
+    padding: vw(3),
+    borderRadius: 16,
+    alignItems: 'center',
+    marginVertical: vh(2),
+  },
+  btnOtptxt: {
+    color: '#1A1A1A',
+    fontWeight: '600',
+    fontSize: 14,
+  },
+  label: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  desp: {
+    color: '#666D80',
+    fontSize: 14,
+    fontWeight: '400',
+  },
+  btnBack: {
+    padding: vw(2),
+    borderWidth: 1,
+    borderColor: '#76787E',
+    borderRadius: 10,
+    alignSelf: 'flex-start',
+  },
 });
