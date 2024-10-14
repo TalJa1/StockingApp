bfd2a36a7442d51d5b4d72def954f23894b4f7a2
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-14 11:50:32 +0700

    Refactor PasswordModifying.tsx: Update UI and email verification logic

--- views/init/PasswordModifying.tsx
+++ views/init/PasswordModifying.tsx
@@ -1,17 +1,71 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  Alert,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBar';
-import {container} from '../../services/styleSheet';
+import {container, vh, vw} from '../../services/styleSheet';
+import {backIcon} from '../../assets/svgXML';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const PasswordModifying = () => {
   useStatusBar('#1A1A1A');
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  const [email, setEmail] = useState('');
+
+  const handleVerify = () => {
+    // Regular expression to validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      // Show an alert in Vietnamese if the email is invalid
+      Alert.alert('Email không hợp lệ', 'Vui lòng nhập địa chỉ email hợp lệ.');
+    } else {
+      // Proceed with the verification process
+      console.log('Email is valid:', email);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <View>
-          <Text>PasswordModifying</Text>
+        <View style={styles.phoneContainer}>
+          <TouchableOpacity
+            style={styles.btnBack}
+            onPress={() => navigation.goBack()}>
+            {backIcon(vw(7), vw(7))}
+          </TouchableOpacity>
+          <View style={{rowGap: vh(1)}}>
+            <Text style={styles.label}>Nhập mã xác minh</Text>
+            <Text style={styles.desp}>
+              Chúng tôi đã gửi mã xác minh đến số điện thoại di động của bạn
+            </Text>
+          </View>
+          <View>
+            <TextInput
+              style={styles.otpInput}
+              placeholder="Email"
+              placeholderTextColor="#76787E"
+              value={email}
+              onChangeText={setEmail}
+            />
+          </View>
+          <TouchableOpacity
+            style={[
+              styles.btnSendOTP,
+              {backgroundColor: email ? '#FFED4B' : '#d3d3d3'},
+            ]}
+            onPress={handleVerify}
+            disabled={!email}>
+            <Text style={styles.btnOtptxt}>Gửi mã xác nhận</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -22,4 +76,48 @@ export default PasswordModifying;
 
 const styles = StyleSheet.create({
   container: container,
+  phoneContainer: {
+    paddingHorizontal: vw(5),
+    flex: 1,
+    paddingVertical: vh(2),
+    rowGap: vh(2),
+  },
+  btnOtptxt: {
+    color: '#1A1A1A',
+    fontWeight: '600',
+    fontSize: 14,
+  },
+  label: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  desp: {
+    color: '#666D80',
+    fontSize: 14,
+    fontWeight: '400',
+  },
+  btnBack: {
+    padding: vw(2),
+    borderWidth: 1,
+    borderColor: '#76787E',
+    borderRadius: 10,
+    alignSelf: 'flex-start',
+  },
+  otpInput: {
+    borderWidth: 1,
+    borderColor: '#76787E',
+    borderRadius: 12,
+    width: '100%',
+    height: vw(15),
+    color: '#76787E',
+    paddingHorizontal: vw(5),
+  },
+  btnSendOTP: {
+    backgroundColor: '#FFED4B',
+    padding: vw(3),
+    borderRadius: 16,
+    alignItems: 'center',
+    marginVertical: vh(2),
+  },
 });
